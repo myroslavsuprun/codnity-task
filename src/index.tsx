77d1b1268363b26216e52b5207a0eec0e5e2172a
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-01-30 13:17:12 +0200

    Added base design architecture; added MUI.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+/**
+ * The starting point of the application.
+ *
+ * Contains style resetting; font addition
+ */
 
-import "./index.css";
+// TODO: update description
 
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
