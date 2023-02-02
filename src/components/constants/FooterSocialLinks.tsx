708004864252a48bc7cacec02438b29e6a75ffcb
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-02-02 22:33:41 +0200

    Corrected social links hrefs.

--- src/components/constants/FooterSocialLinks.tsx
+++ src/components/constants/FooterSocialLinks.tsx
@@ -4,10 +4,13 @@ import {
 } from '@mui/icons-material';
 
 const socialLinks = [
-  { link: 'https://github.com/myroslavsuprun', inner: <LinkedInIcon /> },
+  {
+    link: 'https://github.com/myroslavsuprun',
+    inner: <GitHubIcon />,
+  },
   {
     link: 'https://www.linkedin.com/in/myroslav-suprun/',
-    inner: <GitHubIcon />,
+    inner: <LinkedInIcon />,
   },
 ];
 
