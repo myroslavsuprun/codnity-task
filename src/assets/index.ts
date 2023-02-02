be9bb276740269b1c99eace24a794ce5392a5f84
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-02-02 23:08:04 +0200

    Added layout for About us section.

---
+++ src/assets/index.ts
@@ -0,0 +1,1 @@
+export { default as NasaImage } from './nasa.png';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,20 @@
+import { Grid, Typography } from '@mui/material';
+import { NasaImage } from 'assets';
+
 const Home = () => {
-  return <>Home page </>;
+  return (
+    <>
+      <Grid container p={2}>
+        <Grid item sm={6}>
+          <Typography variant="h4">Powered by</Typography>
+          <Typography variant="h3">NASA API</Typography>
+        </Grid>
+        <Grid item sm={6}>
+          <img width="300" src={NasaImage} alt="Nasa" />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
