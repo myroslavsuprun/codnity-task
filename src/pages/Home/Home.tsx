b685a5b50d6bd22013a5a9d12ee3f31792a5d361
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-02-03 09:58:53 +0200

    Corrected layout for about us section.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,10 +4,21 @@ import { NasaImage } from 'assets';
 const Home = () => {
   return (
     <>
-      <Grid container p={2}>
-        <Grid item sm={6}>
-          <Typography variant="h4">Powered by</Typography>
-          <Typography variant="h3">NASA API</Typography>
+      <Grid container alignItems="center" spacing={4} p={2}>
+        <Grid
+          container
+          spacing={1}
+          justifyContent="flex-end"
+          alignItems="baseline"
+          item
+          sm={6}
+        >
+          <Grid item>
+            <Typography variant="h4">Powered by</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h3">NASA API</Typography>
+          </Grid>
         </Grid>
         <Grid item sm={6}>
           <img width="300" src={NasaImage} alt="Nasa" />
