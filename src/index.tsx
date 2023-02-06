c2c204cf2b879edb78dc07d1dd7ceb5f09e070e1
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-02-06 14:06:12 +0200

    code refactor.

--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/codnity-task">
+      <BrowserRouter basename="/codnity-task/">
         <App />
       </BrowserRouter>
     </ThemeProvider>
