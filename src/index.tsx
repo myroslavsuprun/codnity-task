61b4b8eccc20000ade38b36f6ab554e3a58fd6f2
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-02-06 13:27:13 +0200

    Code refactor.

--- src/index.tsx
+++ src/index.tsx
@@ -31,6 +31,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+// **** Rendering **** //
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
