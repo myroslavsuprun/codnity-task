620da75c97bf57054e1b313028e12186312486e0
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-02-08 17:05:25 +0200

    Code refactor.

--- src/modules/EarthFilterForm/EarthFilterForm.tsx
+++ src/modules/EarthFilterForm/EarthFilterForm.tsx
@@ -4,14 +4,14 @@ import { useGetEarthLandstatQuerySubscription } from 'modules';
 
 // Components
 import {
-  Box,
-  Button,
-  FormControl,
-  InputLabel,
-  Link,
-  OutlinedInput,
-  Stack,
-  TextField,
+    Box,
+    Button,
+    FormControl,
+    InputLabel,
+    Link,
+    OutlinedInput,
+    Stack,
+    TextField,
 } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 
@@ -31,145 +31,149 @@ const coordinatesLink = 'https://www.latlong.net/';
 // **** Component **** //
 
 const EarthFilterForm = ({
-  dateValue,
-  setDateValue,
-  latValue,
-  setLatValue,
-  lonValue,
-  setLonValue,
+    dateValue,
+    setDateValue,
+    latValue,
+    setLatValue,
+    lonValue,
+    setLonValue,
 }: EarthFilterFormProps) => {
-  // ** Hooks
-  const [, setSearchParams] = useSearchParams();
-  const [trigger] = useGetEarthLandstatQuerySubscription();
+    // ** Hooks
+    const [, setSearchParams] = useSearchParams();
+    const [getEarthLandstat] = useGetEarthLandstatQuerySubscription();
 
-  // ** Input handlers
-  const handleDateChange = (newValue: Dayjs | null) => {
-    if (!newValue) return;
+    // ** Input handlers
+    const handleDateChange = (newValue: Dayjs | null) => {
+        if (!newValue) return;
 
-    setDateValue(newValue);
-    updateSearchParams({ date: newValue });
-  };
+        setDateValue(newValue);
+        updateSearchParams({ date: newValue });
+    };
 
-  const handleLatChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
+    const handleLatChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
 
-    setLatValue(value);
-    updateSearchParams({ lat: value });
-  };
+        setLatValue(value);
+        updateSearchParams({ lat: value });
+    };
 
-  const handleLonChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
+    const handleLonChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
 
-    setLonValue(value);
-    updateSearchParams({ lon: value });
-  };
+        setLonValue(value);
+        updateSearchParams({ lon: value });
+    };
 
-  // ** Button & Form handlers
-  const handleMyLocationClick = () => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const { latitude, longitude } = position.coords;
-      // String typization
-      const lat = String(latitude);
-      const lon = String(longitude);
+    // ** Button & Form handlers
+    const handleMyLocationClick = () => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const { latitude, longitude } = position.coords;
+            // String typization
+            const lat = String(latitude);
+            const lon = String(longitude);
 
-      setLatValue(lat);
-      setLonValue(lon);
-      updateSearchParams({ lon, lat });
-    });
-  };
+            setLatValue(lat);
+            setLonValue(lon);
+            updateSearchParams({ lon, lat });
+        });
+    };
 
-  const handleFormSubmit = (e: FormEvent) => {
-    e.preventDefault();
+    const handleFormSubmit = (e: FormEvent) => {
+        e.preventDefault();
 
-    trigger({
-      date: dateValue.format('YYYY-MM-DD'),
-      lat: latValue,
-      lon: lonValue,
-    });
-  };
+        getEarthLandstat({
+            date: dateValue.format('YYYY-MM-DD'),
+            lat: latValue,
+            lon: lonValue,
+        });
+    };
 
-  // ** Uitlity
-  function updateSearchParams({
-    lon = lonValue,
-    lat = latValue,
-    date = dateValue,
-  }) {
-    let formattedDate = defaultDate.format('YYYY-MM-DD');
+    // ** Uitlity
+    function updateSearchParams({
+        lon = lonValue,
+        lat = latValue,
+        date = dateValue,
+    }) {
+        let formattedDate = defaultDate.format('YYYY-MM-DD');
 
-    if (date.isValid()) {
-      formattedDate = date.format('YYYY-MM-DD');
+        if (date.isValid()) {
+            formattedDate = date.format('YYYY-MM-DD');
+        }
+
+        setSearchParams({
+            lon,
+            lat,
+            date: formattedDate,
+        });
     }
 
-    setSearchParams({
-      lon,
-      lat,
-      date: formattedDate,
-    });
-  }
-
-  return (
-    <Stack component="form" noValidate spacing={1} onSubmit={handleFormSubmit}>
-      <Box>
-        <FormControl>
-          <InputLabel htmlFor="latitude">Latitude</InputLabel>
-          <OutlinedInput
-            value={latValue}
-            onChange={handleLatChange}
-            id="latitude"
-            type="number"
-            required
-            label="Latitude"
-          />
-        </FormControl>
-      </Box>
-      <Box>
-        <FormControl>
-          <InputLabel htmlFor="longtitude">Longitude</InputLabel>
-          <OutlinedInput
-            value={lonValue}
-            onChange={handleLonChange}
-            type="number"
-            required
-            id="longitude"
-            label="Longitude"
-          />
-        </FormControl>
-      </Box>
-      <Box>
-        <Button onClick={handleMyLocationClick} type="button" size="small">
-          Use my current location
-        </Button>
-      </Box>
-      <Box>
-        <Link
-          component={Button}
-          size="small"
-          underline="none"
-          href={coordinatesLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Search for specific coordinates
-        </Link>
-      </Box>
-      <Box>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            label="Date picker"
-            inputFormat="YYYY/MM/DD"
-            value={dateValue}
-            onChange={handleDateChange}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </LocalizationProvider>
-      </Box>
-      <Box width={240}>
-        <Button type="submit" variant="contained">
-          Search Earth imagery
-        </Button>
-      </Box>
-    </Stack>
-  );
+    return (
+        <Stack component="form" spacing={1} onSubmit={handleFormSubmit}>
+            <Box>
+                <FormControl>
+                    <InputLabel htmlFor="latitude">Latitude</InputLabel>
+                    <OutlinedInput
+                        value={latValue}
+                        onChange={handleLatChange}
+                        id="latitude"
+                        type="number"
+                        required
+                        label="Latitude"
+                    />
+                </FormControl>
+            </Box>
+            <Box>
+                <FormControl>
+                    <InputLabel htmlFor="longtitude">Longitude</InputLabel>
+                    <OutlinedInput
+                        value={lonValue}
+                        onChange={handleLonChange}
+                        type="number"
+                        required
+                        id="longitude"
+                        label="Longitude"
+                    />
+                </FormControl>
+            </Box>
+            <Box>
+                <Button
+                    onClick={handleMyLocationClick}
+                    type="button"
+                    size="small"
+                >
+                    Use my current location
+                </Button>
+            </Box>
+            <Box>
+                <Link
+                    component={Button}
+                    size="small"
+                    underline="none"
+                    href={coordinatesLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Search for specific coordinates
+                </Link>
+            </Box>
+            <Box>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Date picker"
+                        inputFormat="YYYY/MM/DD"
+                        value={dateValue}
+                        onChange={handleDateChange}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
+            </Box>
+            <Box width={240}>
+                <Button type="submit" variant="contained">
+                    Search Earth imagery
+                </Button>
+            </Box>
+        </Stack>
+    );
 };
 
 // **** Export **** //
